e401887af3e464b3bc2cc56d33650c3de358b0e6
George Marklow <gmarklow87@gmail.com>
2023-05-01 18:22:01 +0100

    Create merge-all.spec.ts

---
+++ rxjs/join/operators/merge-all.spec.ts
@@ -0,0 +1,30 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MyComponent } from './my-component.component';
+import { of } from 'rxjs';
+import { mergeAll } from 'rxjs/operators';
+
+describe('MyComponent', () => {
+  let component: MyComponent;
+  let fixture: ComponentFixture<MyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [MyComponent]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should produce the correct output', () => {
+    const input$ = of(of(1), of(2), of(3));
+    const expectedOutput = [1, 2, 3];
+
+    input$.pipe(mergeAll()).subscribe(output => {
+      expect(output).toEqual(expectedOutput);
+    });
+  });
+});
